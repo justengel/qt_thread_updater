0d3b0ee44e14203dc506f31cd192a48faf7adba7
justengel <jtengel08@gmail.com>
2020-04-09 16:25:41 -0400

    Updated to version 1.0.0

--- qt_thread_updater/__meta__.py
+++ qt_thread_updater/__meta__.py
@@ -1,5 +1,5 @@
 name = 'qt_thread_updater'
-version = '0.0.1'
+version = '1.0.0'
 description = 'Python Qt thread updater to update GUI items using a separate thread.'
 url = 'https://github.com/justengel/qt_thread_updater'
 author = 'Justin Engel'
